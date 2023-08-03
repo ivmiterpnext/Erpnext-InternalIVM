378f5bf6545cc7796fccb6bd6f058135b53372c8
umakanth1999 <umakanth@korecent.com>
2023-08-03 12:36:19 +0530

    title field hidden in lead list_view

--- ivm/public/js/listview/Lead_listview.js
+++ ivm/public/js/listview/Lead_listview.js
@@ -2,5 +2,6 @@ frappe.listview_settings["Lead"] = {
   hide_name_column: true,
   onload: function (me) {
     me.$page.find(`div[data-fieldname='name']`).addClass("hide");
+    me.$page.find(`div[data-fieldname='title']`).addClass("hide");
   },
 };
