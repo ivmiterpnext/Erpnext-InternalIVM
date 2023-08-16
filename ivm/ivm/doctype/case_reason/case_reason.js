cfe18ba428ea5b8b4a48ab068f108686900cc2e8
lavaraju97 <lavaraju@korecent.com>
2023-08-16 11:37:53 +0530

    updatd issue doc

---
+++ ivm/ivm/doctype/case_reason/case_reason.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, korecent and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Case Reason', {
+	// refresh: function(frm) {
+
+	// }
+});
