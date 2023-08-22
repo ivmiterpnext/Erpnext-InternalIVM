5f791a7c5a8725d28db9ade299a6f3e10d4de04d
Abhi <abhimanyu.d@korecent.com>
2023-08-22 18:26:57 +0530

    deleted custom folder & added fixtures of custom-fields

--- ivm/hooks.py
+++ ivm/hooks.py
@@ -9,7 +9,7 @@ app_license = "MIT"
 
 # Includes in <head>
 # ------------------
-fixtures = [{"doctype": "Issue Type", "filters": {"name": ["in", ["Desktop Support"]]}}]
+fixtures = ["Custom Field"]
 # include js, css files in header of desk.html
 # app_include_css = "/assets/ivm/css/ivm.css"
 # app_include_js = "/assets/ivm/js/ivm.js"
