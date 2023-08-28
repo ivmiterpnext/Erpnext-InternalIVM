fb4099470be3f1a1f17712397346ab4367bd5e70
sahil <sahil@korecent.com>
2023-08-28 14:15:22 +0000

    calculation fixes

--- ivm/controllers/project.py
+++ ivm/controllers/project.py
@@ -6,131 +6,138 @@ import math
 
 
 class CustomProjectController(OriginalProjectController):
-    def validate(self):
-        OriginalProjectController.validate(self)
-        self.update_provide_planogram_due_date()
-        self.update_approve_planogram_and_locker_config_due()
-        self.update_pog_created_in_database_due()
-        self.update_sample_products_due()
-        self.update_delivery_and_install_contact_due()
-    def update_delivery_and_install_contact_due(self):
-        if not self.placement_agreement:
-            self.graphic_design_approval_due = ""
-            self.delivery_and_install_contact_due_customs =""
-            self.delivery_install_and_coi_requirements=""
-            self.user_and_restriction_requirements_due = ""
-            return
-        user_and_restriction_requirements = user_and_restriction_requirements_due(self.placement_agreement, self.added_days, self.expedited_delivery)
-        weekday = getdate(self.placement_agreement).weekday()
-        if self.expedited_delivery:
-            base_days = 9
-            days = calculate_days(int(self.added_days), weekday, base_days)
-            due_days = add_days(self.placement_agreement, days)
-            self.graphic_design_approval_due = due_days
-            self.delivery_and_install_contact_due_customs =due_days
-            self.delivery_install_and_coi_requirements=due_days
-            self.user_and_restriction_requirements_due = user_and_restriction_requirements
-        else:
-            base_days = 10
-            days = calculate_days(int(self.added_days), weekday, base_days)
-            due_days = add_days(self.placement_agreement, days)
-            self.delivery_and_install_contact_due_customs =due_days
-            self.delivery_install_and_coi_requirements=due_days
-            self.user_and_restriction_requirements_due = user_and_restriction_requirements
+	def validate(self):
+		OriginalProjectController.validate(self)
+		self.update_provide_planogram_due_date()
+		self.update_approve_planogram_and_locker_config_due()
+		self.update_pog_created_in_database_due()
+		self.update_sample_products_due()
+		self.update_delivery_and_install_contact_due()
 
-            base_days1 = 20
-            days1= calculate_days(int(self.added_days), weekday, base_days1)
-            self.graphic_design_approval_due = add_days(self.placement_agreement, days1)
+	def update_delivery_and_install_contact_due(self):
+		if not self.placement_agreement:
+			self.graphic_design_approval_due = ""
+			self.delivery_and_install_contact_due_customs =""
+			self.delivery_install_and_coi_requirements=""
+			self.user_and_restriction_requirements_due = ""
+			return
 
-    def update_sample_products_due(self):
-        if not self.placement_agreement:
-            self.sample_products_due = ''
-            self.sample_badge_due = ''
-            return
-        base_days = 2 if (self.expedited_delivery) else (7 if self.locale and self.locale == "Domestic" else 13)
-        added_days = int(self.added_days) if self.added_days else 0
-        due_date = get_sample_products_due_date(self.placement_agreement, base_days, added_days)
-        self.sample_products_due = due_date
-        self.sample_badge_due = due_date
+		user_and_restriction_requirements = user_and_restriction_requirements_due(self.placement_agreement, self.added_days, self.expedited_delivery)
+		weekday = getdate(self.placement_agreement).weekday()
+		
+		if self.expedited_delivery:
+			base_days = 9
+			days = calculate_days(int(self.added_days), weekday, base_days)
+			due_days = add_days(self.placement_agreement, days)
+			self.graphic_design_approval_due = due_days
+			self.delivery_and_install_contact_due_customs =due_days
+			self.delivery_install_and_coi_requirements=due_days
+			self.user_and_restriction_requirements_due = user_and_restriction_requirements
+		else:
+			base_days = 10
+			days = calculate_days(int(self.added_days), weekday, base_days)
+			due_days = add_days(self.placement_agreement, days)
+			self.delivery_and_install_contact_due_customs =due_days
+			self.delivery_install_and_coi_requirements=due_days
+			self.user_and_restriction_requirements_due = user_and_restriction_requirements
 
-    def update_provide_planogram_due_date(self):
-        if not self.placement_agreement:
-            return
+			base_days1 = 20
+			days1= calculate_days(int(self.added_days), weekday, base_days1)
+			self.graphic_design_approval_due = add_days(self.placement_agreement, days1)
 
-        base_days = 7 if (self.expedited_delivery) else (17 if self.locale and self.locale == "Domestic" else 23)
-        added_days = int(self.added_days) if self.added_days else 0
-        self.provide_planogram_due = get_provide_planogram_base_date(self.placement_agreement, base_days, added_days)
+	def update_sample_products_due(self):
+		if not self.placement_agreement:
+			self.sample_products_due = ''
+			self.sample_badge_due = ''
+			return
+		
+		base_days = 2 if (self.expedited_delivery) else (7 if self.locale and self.locale == "Domestic" else 13)
+		added_days = int(self.added_days) if self.added_days else 0
+		due_date = get_sample_products_due_date(self.placement_agreement, base_days, added_days)
+		self.sample_products_due = due_date
+		self.sample_badge_due = due_date
 
-    def update_approve_planogram_and_locker_config_due(self):
-        if not self.placement_agreement:
-            return
+	def update_provide_planogram_due_date(self):
+		if not self.placement_agreement:
+			self.provide_planogram_due = ''
+			return
 
-        base_days = 11 if (self.expedited_delivery) else (25 if self.locale and self.locale == "Domestic" else 31)
-        added_days = int(self.added_days) if self.added_days else 0
-        self.approve_planogram_and_locker_config_due = get_provide_planogram_base_date(self.placement_agreement, base_days, added_days)
+		base_days = 7 if (self.expedited_delivery) else (17 if self.locale and self.locale == "Domestic" else 23)
+		added_days = int(self.added_days) if self.added_days else 0
+		self.provide_planogram_due = get_provide_planogram_base_date(self.placement_agreement, base_days, added_days)
 
-    def update_pog_created_in_database_due(self):
-        if not self.placement_agreement:
-            return
+	def update_approve_planogram_and_locker_config_due(self):
+		if not self.placement_agreement:
+			self.approve_planogram_and_locker_config_due = ''
+			return
 
-        base_days = 10 if (self.expedited_delivery) else (24 if self.locale and self.locale == "Domestic" else 27)
-        added_days = int(self.added_days) if self.added_days else 0
-        self.pog_created_in_database_due = get_provide_planogram_base_date(self.placement_agreement, base_days, added_days)
+		base_days = 11 if (self.expedited_delivery) else (25 if self.locale and self.locale == "Domestic" else 31)
+		added_days = int(self.added_days) if self.added_days else 0
+		self.approve_planogram_and_locker_config_due = get_provide_planogram_base_date(self.placement_agreement, base_days, added_days)
+
+	def update_pog_created_in_database_due(self):
+		if not self.placement_agreement:
+			self.pog_created_in_database_due = ''
+			return
+
+		base_days = 10 if (self.expedited_delivery) else (24 if self.locale and self.locale == "Domestic" else 27)
+		added_days = int(self.added_days) if self.added_days else 0
+		self.pog_created_in_database_due = get_provide_planogram_base_date(self.placement_agreement, base_days, added_days)
 
 
 def get_sample_products_due_date(placement_agreement, base_days, added_days):
-    weekday = getdate(placement_agreement).weekday()
-    if weekday == 6:
-        return add_days(placement_agreement, base_days + added_days + math.ceil(((base_days + added_days) / 5) * 2))
-    elif weekday == 5:
-        return add_days(placement_agreement, - base_days - added_days -0 + base_days + math.ceil(((base_days + added_days) / 5) * 2))
-    else:
-        return add_days(placement_agreement, base_days + added_days + int(math.floor((base_days + added_days + {0: -1, 1: 0, 2: 1, 3: 2, 4: 3}.get(weekday, 0)) / 5) * 2))
-    
+	weekday = getdate(placement_agreement).weekday()
+	if weekday == 6:
+		return add_days(placement_agreement, base_days + added_days + math.ceil(((base_days + added_days) / 5) * 2))
+	elif weekday == 5:
+		return add_days(placement_agreement, - base_days - added_days - 0 + base_days + math.ceil(((base_days + added_days) / 5) * 2))
+	else:
+		return add_days(placement_agreement, base_days + added_days + int(math.floor((base_days + added_days + {0: -1, 1: 0, 2: 1, 3: 2, 4: 3}.get(weekday, 0)) / 5) * 2))
+	
 def user_and_restriction_requirements_due(placement_agreement, added_days, expedited_delivery):
-    weekday = getdate(placement_agreement).weekday()
-    if expedited_delivery:
-        base_days = 14
-        days = calculate_days(int(added_days), weekday, base_days)
-        return add_days(placement_agreement, days)
-    else:
-        base_days = 20
-        days = calculate_days(int(added_days), weekday, base_days)
-        return add_days(placement_agreement, days)
+	weekday = getdate(placement_agreement).weekday()
+	if expedited_delivery:
+		base_days = 14
+		days = calculate_days(int(added_days), weekday, base_days)
+		return add_days(placement_agreement, days)
+	else:
+		base_days = 20
+		days = calculate_days(int(added_days), weekday, base_days)
+		return add_days(placement_agreement, days)
 
 
-# preparing date based on the placement agreement, base date and added days data #
+# preparing date based on the placement agreement, base days and added days data #
 def get_provide_planogram_base_date(date, base_days, added_days):
-    try:
-        no_of_days = base_days + get_no_days(date, base_days, added_days)
-        return add_days(date, no_of_days)
-    except Exception as e:
-        frappe.msgprint(e)
+	try:
+		no_of_days = base_days + added_days + get_no_days(date, base_days, added_days)
+		return add_days(date, no_of_days)
+	except Exception as e:
+		frappe.msgprint(e)
 
 def get_no_days(date, base_days, added_days):
-    weekday = getdate(date).weekday()
+	weekday = getdate(date).weekday()
 
-    if (weekday == 5):
-        return int(math.ceil(((base_days + added_days)/5)*2))
-    elif (weekday == 6):
-        return (- base_days - added_days + base_days + int(math.ceil(((base_days + added_days)/5)*2)))
-    else:
-        days_maps = {0: -1, 1: 0, 2: 1, 3: 2, 4:3}
-        return int((math.floor(base_days + added_days + days_maps.get(weekday))/5)*2)
+	if (weekday == 5):
+		return int(math.ceil(((base_days + added_days)/5)*2))
+	elif (weekday == 6):
+		return (- base_days - added_days - 1 + base_days + int(math.ceil(((base_days + added_days)/5)*2)))
+	else:
+		days_maps = {0: -1, 1: 0, 2: 1, 3: 2, 4:3}
+		return int((math.floor(base_days + added_days + days_maps.get(weekday))/5)*2)
 
 def calculate_days(added_days, weekday, base_days):
-    if weekday == 4:  # Friday
-        days = base_days + added_days + \
-            int(math.ceil(((base_days + added_days) / 5) * 2))
-    elif weekday == 5:  # Saturday
-        days = -1 + base_days + added_days + \
-            int(math.ceil(((base_days + added_days) / 5) * 2))
-    elif weekday == 6:  # Sunday
-        days = base_days + added_days + \
-            int(math.floor(((base_days + added_days - 1) / 5) * 2))
-    else:
-        days = base_days + added_days + \
-            int(math.floor(((base_days + added_days + weekday) / 5) * 2))
-    return days
+	if weekday == 4:  # Friday
+		days = base_days + added_days + \
+			int(math.ceil(((base_days + added_days) / 5) * 2))
+	elif weekday == 5:  # Saturday
+		days = -1 + base_days + added_days + \
+			int(math.ceil(((base_days + added_days) / 5) * 2))
+	elif weekday == 6:  # Sunday
+		days = base_days + added_days + \
+			int(math.floor(((base_days + added_days - 1) / 5) * 2))
+	else:
+		days = base_days + added_days + \
+			int(math.floor(((base_days + added_days + weekday) / 5) * 2))
+	return days
 
 ############################# end of calculations ################################
