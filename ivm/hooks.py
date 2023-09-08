4a7b522acc46a1e08933c93e6f73843b7c2fbc1b
Abhi <abhimanyu.d@korecent.com>
2023-09-08 11:35:33 +0530

    given role and permission for doctype

--- ivm/hooks.py
+++ ivm/hooks.py
@@ -9,7 +9,7 @@ app_license = "MIT"
 
 # Includes in <head>
 # ------------------
-fixtures = ["Custom Field"]
+fixtures = [ { "doctype": "Custom DocPerm", "filters": { "role": [ "in", ["Graphic Designer"] ] } }]
 # include js, css files in header of desk.html
 # app_include_css = "/assets/ivm/css/ivm.css"
 # app_include_js = "/assets/ivm/js/ivm.js"
