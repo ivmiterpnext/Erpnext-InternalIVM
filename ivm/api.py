09bd3db03a5a0ca202c3f3a9e89d1a2e6d400c89
Abhi <abhimanyu.d@korecent.com>
2023-10-06 12:57:54 +0530

    adding attachments links from communication to linked communication to issue record

--- ivm/api.py
+++ ivm/api.py
@@ -239,6 +239,7 @@ def on_session_creation():
 @frappe.whitelist()
 def creating_issue(doc, method):
     try:
+        attachments = doc.get_attachments()
         message = doc.content
         # getting record matching to email received
         email = None
@@ -256,6 +257,18 @@ def creating_issue(doc, method):
             issue_name.description = message
             issue_name.save()
             frappe.db.commit()
+            # if attachments are in received emails then moving it to linked issue record-----------------
+            if (attachments):
+                file_urls = [url['file_url'] for url in attachments]
+                for links in file_urls:
+                    doc = frappe.get_doc({
+                        'doctype': 'File',
+                        'is_private': 1,
+                        'file_url': links,
+                        'attached_to_doctype': 'Issue',
+                        'attached_to_name': doc.reference_name
+                    }).save()
+                    frappe.db.commit()
     except Exception as e:
         pass
 
