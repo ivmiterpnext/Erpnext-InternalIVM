454a3911811b216fe21dcd7050dc2af4c15ce503
Abhi <abhimanyu.d@korecent.com>
2023-10-12 13:02:42 +0530

    resolved popup error in project and added alerts when WR creates

--- ivm/public/js/doctype/Project.js
+++ ivm/public/js/doctype/Project.js
@@ -18,29 +18,29 @@ frappe.ui.form.on("Project", {
 
     $(document).ready(function () {
       const fieldGroups = {
-                      input: ['number_of_kiosks', 'opportunity', "po_and_tracking", "associated_deployment_location", "customer"],
-                      select: ["install_type", "vault_size", "kiosk_options", "kvm_switch_options", "network_options", "countertop_color", "ada_side_table", "kiosk_side_for_table"],
-                      div: ["expedited_delivery", "enhanced_lockers", "number_of_machines", "number_of_primary_lockers", "number_of_secondary_lockers", "number_of_vaults", "opportunity_term"],
-                      textarea: ["description", "vault_power_configuration_details", "expedited_delivery_details"]
-                    };
-                    
-                    Object.keys(fieldGroups).forEach(group => {
-                      fieldGroups[group].forEach(field => {
-                        let selector;
-                        let backgroundColor = '#e1f0f0';
-                    
-                        if (group === 'div' && (field === "expedited_delivery" || field === "enhanced_lockers")) {
-                          selector = `div[data-fieldname="${field}"] .label-area`;
-                        } else {
-                          selector = `${group}[data-fieldname="${field}"]`;
-                          if (group === 'div' && field !== "expedited_delivery" && field !== "enhanced_lockers") {
-                            selector = `div[data-fieldname="${field}"] .control-value.like-disabled-input`;
-                          }
-                        }
-                    
-                        $(selector).css('background-color', backgroundColor);
-                      });
-                    });
+        input: ['number_of_kiosks', 'opportunity', "po_and_tracking", "associated_deployment_location", "customer"],
+        select: ["install_type", "vault_size", "kiosk_options", "kvm_switch_options", "network_options", "countertop_color", "ada_side_table", "kiosk_side_for_table"],
+        div: ["expedited_delivery", "enhanced_lockers", "number_of_machines", "number_of_primary_lockers", "number_of_secondary_lockers", "number_of_vaults", "opportunity_term"],
+        textarea: ["description", "vault_power_configuration_details", "expedited_delivery_details"]
+      };
+
+      Object.keys(fieldGroups).forEach(group => {
+        fieldGroups[group].forEach(field => {
+          let selector;
+          let backgroundColor = '#e1f0f0';
+
+          if (group === 'div' && (field === "expedited_delivery" || field === "enhanced_lockers")) {
+            selector = `div[data-fieldname="${field}"] .label-area`;
+          } else {
+            selector = `${group}[data-fieldname="${field}"]`;
+            if (group === 'div' && field !== "expedited_delivery" && field !== "enhanced_lockers") {
+              selector = `div[data-fieldname="${field}"] .control-value.like-disabled-input`;
+            }
+          }
+
+          $(selector).css('background-color', backgroundColor);
+        });
+      });
     });
     if (frm.doc.__islocal) {
       if (frm.doc.customer) {
@@ -54,7 +54,7 @@ frappe.ui.form.on("Project", {
       frm.set_value(
         "number_of_lockers",
         (frm.doc.number_of_primary_lockers || 0) +
-          (frm.doc.number_of_secondary_lockers || 0)
+        (frm.doc.number_of_secondary_lockers || 0)
       );
     }
   },
@@ -68,43 +68,43 @@ frappe.ui.form.on("Project", {
     }
   },
   project_type: function (frm) {
-    if (frm.doc.connectivity_type){
+    if (frm.doc.connectivity_type) {
       frappe.call({
-          method: 'ivm.api.get_connectivity_type_record',
-          args: {
-              record_name: frm.doc.connectivity_type
-          },
-          callback: function(r) {
-              const message = r.message;
-              if (message) {
-                  if (message.cell_carrier) {
-                      updateSelectFieldOptions(frm, 'cell_carrier', message.cell_carrier);
-                  }
-              }
-          }
-      });
-  }
-  },
-  connectivity_type: function (frm) {
-  if (frm.doc.connectivity_type){
-  frappe.call({
-      method: 'ivm.api.get_connectivity_type_record',
-      args: {
+        method: 'ivm.api.get_connectivity_type_record',
+        args: {
           record_name: frm.doc.connectivity_type
-      },
-      callback: function(r) {
+        },
+        callback: function (r) {
           const message = r.message;
           if (message) {
-              if (message.cell_carrier) {
-                  updateSelectFieldOptions(frm, 'cell_carrier', message.cell_carrier);
-              }
+            if (message.cell_carrier) {
+              updateSelectFieldOptions(frm, 'cell_carrier', message.cell_carrier);
+            }
           }
-      }
-  });
-}
+        }
+      });
+    }
+  },
+  connectivity_type: function (frm) {
+    if (frm.doc.connectivity_type) {
+      frappe.call({
+        method: 'ivm.api.get_connectivity_type_record',
+        args: {
+          record_name: frm.doc.connectivity_type
+        },
+        callback: function (r) {
+          const message = r.message;
+          if (message) {
+            if (message.cell_carrier) {
+              updateSelectFieldOptions(frm, 'cell_carrier', message.cell_carrier);
+            }
+          }
+        }
+      });
+    }
   },
   opportunity: function (frm) {
-    frappe.db.get_value("Opportunity", frm.doc.opportunity, "*",  (response) =>  {
+    frappe.db.get_value("Opportunity", frm.doc.opportunity, "*", (response) => {
       var doc = response;
       var fieldsToSet = [
         "number_of_kiosks",
@@ -129,17 +129,17 @@ frappe.ui.form.on("Project", {
         "customer",
       ];
       for (var field of fieldsToSet) {
-        if (doc[field]!== undefined) {
+        if (doc[field] !== undefined) {
           frm.set_value(field, doc[field]);
         }
       }
       frm.set_value(
         "number_of_lockers",
         (doc.number_of_primary_lockers || 0) +
-          (doc.number_of_secondary_lockers || 0)
+        (doc.number_of_secondary_lockers || 0)
       );
       frm.set_value("opportunity_term", doc.sv_term);
-      frm.set_value("associated_deployment_location",doc.deployment_address)
+      frm.set_value("associated_deployment_location", doc.deployment_address)
 
       if (doc.customer_name) {
         frappe.db.get_value(
@@ -182,6 +182,23 @@ frappe.ui.form.on("Project", {
   },
   contact_name: function (frm) {
     fetchContactDetails(frm, "contact_name", "contact_phone", "contact_email");
+  },
+  after_save(frm) {
+    if (frm.doc.graphic_design_approved_date) {
+      show_Dialog('Wrap Ready', frm);
+    }
+    if (frm.doc.planogram_approved_date) {
+      createWarehouseRequest('Build Machine', null, frm);
+    }
+    if (frm.doc.locker_configuration_approved_date) {
+      createWarehouseRequest('Build Locker', null, frm);
+    }
+    if (frm.doc.kiosk_configuration_approved_date) {
+      createWarehouseRequest('Build Kiosk', null, frm);
+    }
+    if (frm.doc.vault_configuration_approved_date) {
+      createWarehouseRequest('Build Vault', null, frm);
+    }
   },
 });
 
@@ -228,30 +245,10 @@ function fetchContactDetails(frm, contactField, phoneField, emailField) {
 
 function updateSelectFieldOptions(frm, fieldname, optionsData) {
   const optionsList = optionsData.map(option => option[fieldname]);
-  frm.set_df_property(fieldname, 'options', optionsList); 
+  frm.set_df_property(fieldname, 'options', optionsList);
   frm.refresh_field(fieldname);
 }
 
-frappe.ui.form.on('Project', {
-  before_save(frm) {
-    console.log("srdtyfugkiy")
-    if (frm.doc.graphic_design_approved_date) {
-      show_Dialog('Wrap Ready',frm);
-    }
-    if (frm.doc.planogram_approved_date) {
-      createWarehouseRequest('Build Machine', null, frm);
-    }
-    if (frm.doc.locker_configuration_approved_date) {
-      createWarehouseRequest('Build Locker', null, frm);
-    }
-    if (frm.doc.kiosk_configuration_approved_date) {
-      createWarehouseRequest('Build Kiosk', null, frm);
-    }
-    if (frm.doc.vault_configuration_approved_date) {
-      createWarehouseRequest('Build Vault', null, frm);
-    }
-  },
-});
 
 function show_Dialog(reason, frm) {
   const dialog = new frappe.ui.Dialog({
@@ -264,43 +261,28 @@ function show_Dialog(reason, frm) {
         options: 'Yes\nNo',
         default: 'No',
       },
+      {
+        fieldname: 'attach_files',
+        fieldtype: 'Attach',
+        depends_on: 'eval:doc.add_attachments=="Yes"',
+      },
     ],
     primary_action_label: 'Submit',
     primary_action(values) {
-      const addAttachments = values.add_attachments;
-      dialog.hide();
-
-      if (addAttachments === 'Yes') {
-        show_Attachment_Dialog(reason, frm);
-      } else {
-        createWarehouseRequest(frm,reason, null);
+      const addAttachments = values.attach_files;
+      if (addAttachments) {
+        createWarehouseRequest(reason, addAttachments, frm);
       }
+      else{
+        createWarehouseRequest(reason, null, frm);
+      }
+      dialog.hide();
     },
   });
 
   dialog.show();
 }
 
-function show_Attachment_Dialog(reason, frm) {
-  const attachmentDialog = new frappe.ui.Dialog({
-    title: 'Attach Files',
-    fields: [
-      {
-        label: 'Attach Files',
-        fieldname: 'attach_files',
-        fieldtype: 'Attach',
-      },
-    ],
-    primary_action_label: 'Submit',
-    primary_action(values) {
-      const attachedFiles = values.attach_files;
-      attachmentDialog.hide();
-      createWarehouseRequest(frm, reason, attachedFiles);
-    },
-  });
-
-  attachmentDialog.show();
-}
 
 function createWarehouseRequest(reason, attachedFiles, frm) {
   const doc = frm.doc;
@@ -311,6 +293,41 @@ function createWarehouseRequest(reason, attachedFiles, frm) {
       doc: doc,
       reason: reason,
       attached_files: attachedFiles,
+    },
+    callback: function (r) {
+      frm.reload_doc()
+      var reason = r.message
+      if (reason === "Wrap Ready") {
+        //show_alert with indicator
+        frappe.show_alert({
+          message: __(`Warehouse Request <b><span style="color: #FF5733;">${reason} </span> is created`),
+          indicator: 'green'
+        }, 5);
+      }
+      else if (reason === "Build Machine") {
+        frappe.show_alert({
+          message: __(`Warehouse Request <b><span style="color: #FF5733;">${reason} </span> is created`),
+          indicator: 'green'
+        }, 5);
+      }
+      else if (reason === "Build Locker") {
+        frappe.show_alert({
+          message: __(`Warehouse Request <b><span style="color: #FF5733;">${reason} </span> is created`),
+          indicator: 'green'
+        }, 5);
+      }
+      else if (reason === "Build Kiosk") {
+        frappe.show_alert({
+          message: __(`Warehouse Request <b><span style="color: #FF5733;">${reason} </span> is created`),
+          indicator: 'green'
+        }, 5);
+      }
+      else if (reason === "Build Vault") {
+        frappe.show_alert({
+          message: __(`Warehouse Request <b><span style="color: #FF5733;">${reason} </span> is created`),
+          indicator: 'green'
+        }, 5);
+      }
     }
   });
 }
