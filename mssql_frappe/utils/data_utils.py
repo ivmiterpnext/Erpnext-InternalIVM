d7ef1d25ea5dadb92d4d01c920ef191df1d5780d
Landon Hammond <lhammond@ivminc.com>
2025-10-10 13:49:41 -0400

    potential fix

--- mssql_frappe/utils/data_utils.py
+++ mssql_frappe/utils/data_utils.py
@@ -49,6 +49,10 @@ def set_attrs_from_dict(obj, data, child_table_map=None):
 
         setattr(obj, mapped_key, v)
 
+        for field in obj.__dict__:
+            if getattr(obj, field) == "":
+                setattr(obj, field, None)
+
 def normalize_child_table_field(value, child_field):
     rows = []
 
