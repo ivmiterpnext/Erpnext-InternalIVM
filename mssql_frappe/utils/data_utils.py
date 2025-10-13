812c82abf1c50ba5c8b76178f83185323584076d
Landon Hammond <lhammond@ivminc.com>
2025-10-13 15:38:38 -0400

    Attribute error fix

--- mssql_frappe/utils/data_utils.py
+++ mssql_frappe/utils/data_utils.py
@@ -38,7 +38,7 @@ def set_attrs_from_dict(obj, data, child_table_map=None):
         if mapped_key in child_table_map:
             child_field = child_table_map[mapped_key]
             rows = normalize_child_table_field(v, child_field)
-            obj.set(mapped_key, rows)
+            obj.set(mapped_key, [frappe._dict(row) for row in rows])
             continue
 
         if mapped_key.endswith("id") and not isinstance(v, (list, dict)):
