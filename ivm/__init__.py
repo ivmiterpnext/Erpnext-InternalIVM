b89cd7701c69fbdcf9c8a0a93c5b70544940aea0
Landon Hammond <lhammond@ivminc.com>
2026-02-11 13:43:33 -0500

    After v16 migration

--- ivm/__init__.py
+++ ivm/__init__.py
@@ -1,8 +1,14 @@
 
 __version__ = '0.0.1'
 
-from frappe.email.doctype.email_account.email_account import EmailAccount
-from ivm.api import send_auto_reply
+def setup_monkey_patches():
+	from frappe.email.doctype.email_account.email_account import EmailAccount
+	from ivm.api import send_auto_reply
+	
+	EmailAccount.send_auto_reply = send_auto_reply
 
-
-EmailAccount.send_auto_reply = send_auto_reply
+try:
+	setup_monkey_patches()
+except ImportError:
+	# During installation, frappe might not be available yet
+	pass
