0530058feebf6ad0ced96ee125a1ea80c9d3fc5a
Landon Hammond <lhammond@ivminc.com>
2026-02-16 16:02:41 -0500

    f

--- ivm/client_management/doctype/address_link/address_link_list.js
+++ ivm/client_management/doctype/address_link/address_link_list.js
@@ -2,7 +2,7 @@ frappe.listview_settings['Address Link'] = {
     refresh: function(listview) {
         listview.page.add_inner_button("Sync", function() {
             frappe.call({
-                method: "ivm.machine_hardware_management.doctype.address_link.address_link.sync",
+                method: "ivm.client_management.doctype.address_link.address_link.sync",
                 callback: function(r) {
                     frappe.msgprint(r.message || "Sync complete");
                 }
--- ivm/client_management/doctype/client_link/client_link_list.js
+++ ivm/client_management/doctype/client_link/client_link_list.js
@@ -2,7 +2,7 @@ frappe.listview_settings['Client Link'] = {
     refresh: function(listview) {
         listview.page.add_inner_button("Sync", function() {
             frappe.call({
-                method: "ivm.machine_hardware_management.doctype.client_link.client_link.sync",
+                method: "ivm.client_management.doctype.client_link.client_link.sync",
                 callback: function(r) {
                     frappe.msgprint(r.message || "Sync complete");
                 }
